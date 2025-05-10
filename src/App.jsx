641b4af35a36fcc5324f1ada665a5a5f32ae1765
caiocroccia22 <caiocroccia22@gmail.com>
2025-05-10 13:19:55 -0300

    feat: Implement layout to show git hub user profile

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Perfil from "./components/Perfil/Perfil";
 import ReposList from "./components/ReposList";
 import Title from "./components/Title/Title";
+import styles from './App.module.css';
+
+
 
 // Codigo do componente
 
@@ -15,13 +18,21 @@ function App() {
  return (
   <>
     <Title />
-    <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+    <div className="container">
+      <div className="content-mensage">
+        <h2 className={`${styles.secondaryTitle}`}>
+          Digite o seu usuário do Github
+        </h2>
+        <input className={`${styles.contentInput}`} type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+      </div>
+      <div className={`${styles.contentProfile}`}>
+        <Perfil className={`${styles.Profile}`} nomeUsuario={nomeUsuario} />
+      </div>
+    </div>
     {/* Esses atributos passados como string são acessados dentro dos componentes */}
     
     {nomeUsuario.length > 4 && (
       <>  
-        <Title />
-        <Perfil nomeUsuario={nomeUsuario} />
         <ReposList nomeUsuario={nomeUsuario}/>
       </>
     )}
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,16 +3,22 @@ import styles from "./Title.module.css"
 
 const Title = () => {
     return(
-        <header>
-            <h1>Page</h1>
-            <ul class="nav nav-underline">
-                <li className={`${styles.itemLink} nav-item`} >
-                    <a className={`nav-link`} aria-current="page" href="#">Consultar conta Git</a>
-                </li>
-                <li className={`${styles.itemLink} nav-item`}>
-                    <a className={`${styles.itemLink}`} href="#">Não tenho conta no Github</a>
-                </li>
-            </ul>
+        <header className={`${styles.header}`}>
+            <nav className={` nav ${styles.nav}`}>
+                <h1 className={`${styles.title}`}>Bem vindo</h1>
+                <ul className={`${styles.navList}`}>
+                    <li className={`${styles.itemLink} nav-item`} >
+                        <a className={`nav-link`} aria-current="page" href="#">Consultar conta Git</a>
+                    </li>
+                    <li className={`${styles.itemLink} nav-item`}>
+                        <button className={`btn ${styles.btn}`}>
+                            <a className={`${styles.itemLink}`} href="#">
+                                Não tenho conta no Github
+                            </a>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 }
